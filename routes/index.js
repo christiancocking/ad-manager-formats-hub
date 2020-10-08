ef7ddcfe3e47990d4cd97dc171892b52db28027f
christianc750 <christianc750@gmail.com>
2020-10-08 07:53:47 -0500

    Adding an index route

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/300x250', function(req, res, next) {
   res.render('300x250', { title: '300x250 Example' });
 });
 
-/* GET home page. */
 router.get('/mwi', function(req, res, next) {
   res.render('mwi', { title: 'Mobile Web Insterstial' });
 });
 
-/* GET home page. */
 router.get('/range-size', function(req, res, next) {
   res.render('range-size', { title: 'Ranged Size Tagging Example' });
 });
 
-/* GET home page. */
 router.get('/web-anchor', function(req, res, next) {
   res.render('web-anchor', { title: 'Mobile Web Anchor' });
 });
+
+router.get('/', function(req, res, next) {
+  console.log(req.headers);
+  let ldap = req.headers['x-appengine-user-nickname'] || 'localhost';
+  res.render('index', { username: ldap });
+});
+
+
 
 
 module.exports = router;
